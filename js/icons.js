c3d908e31cf4ce23db45627ab2eea00e4d55db23
Franklin <86998577+f-mael@users.noreply.github.com>
2026-05-21 22:44:01 -0600

    Update icons.js

--- js/icons.js
+++ js/icons.js
@@ -1,53 +1,7 @@
 // Font Awesome Icon Mapping for Fallout Shelter Resources
-// Using Font Awesome 6 icons
+const RESOURCE_ICONS = {"caps":"fa-coins","nuka":"fa-flask","food":"fa-utensils","water":"fa-droplet","energy":"fa-bolt","power":"fa-bolt","stimpack":"fa-heart-pulse","radaway":"fa-flask-vial","lunchbox":"fa-box","mrhandy":"fa-robot","petcarrier":"fa-dog","starter":"fa-gift","default":"fa-cube"};
+const RESOURCE_FIELD_ICONS = {"Caps":"caps","NukaColaQuantum":"nuka","Food":"food","Water":"water","Energy":"energy","Power":"power","StimPack":"stimpack","RadAway":"radaway","MrHandy":"mrhandy","PetCarrier":"petcarrier","Lunchboxes":"lunchbox","LunchBoxesCount":"lunchbox"};
 
-const RESOURCE_ICONS = {
-    // Resources - Font Awesome icons
-    'caps': 'fa-coins',
-    'nuka': 'fa-flask',
-    'food': 'fa-utensils',
-    'water': 'fa-droplet',
-    'energy': 'fa-bolt',
-    'power': 'fa-bolt',
-    'stimpack': 'fa-heart-pulse',
-    'radaway': 'fa-flask-vial',
-    
-    // Special Items
-    'lunchbox': 'fa-box',
-    'mrhandy': 'fa-robot',
-    'petcarrier': 'fa-dog',
-    'starter': 'fa-gift',
-    
-    // Default fallback
-    'default': 'fa-cube'
-};
-
-// Map resource field names to icon keys
-const RESOURCE_FIELD_ICONS = {
-    'Caps': 'caps',
-    'NukaColaQuantum': 'nuka',
-    'Food': 'food',
-    'Water': 'water',
-    'Energy': 'energy',
-    'Power': 'power',
-    'StimPack': 'stimpack',
-    'RadAway': 'radaway',
-    'MrHandy': 'mrhandy',
-    'PetCarrier': 'petcarrier',
-    'Lunchboxes': 'lunchbox',
-    'LunchBoxesCount': 'lunchbox'
-};
-
-function getResourceIcon(fieldName) {
-    const iconKey = RESOURCE_FIELD_ICONS[fieldName] || fieldName.toLowerCase();
-    return RESOURCE_ICONS[iconKey] || RESOURCE_ICONS['default'];
-}
-
-function getIconHtml(fieldName, className = '') {
-    const iconClass = getResourceIcon(fieldName);
-    return `<i class="fas ${iconClass} ${className}" title="${fieldName}"></i>`;
-}
-
-function getIcon(fieldName) {
-    return getResourceIcon(fieldName);
-}
+const getResourceIcon = field => RESOURCE_ICONS[RESOURCE_FIELD_ICONS[field] || field.toLowerCase()] || RESOURCE_ICONS['default'];
+const getIconHtml = (field, cls = '') => `<i class="fas ${getResourceIcon(field)} ${cls}" title="${field}"></i>`;
+const getIcon = getResourceIcon;
